2ebc7de8debde7bb704eeece769b99d9ad6c9913
zwhlt007 <42463094+zwhlt007@users.noreply.github.com>
2019-04-20 13:49:36 +0800

    Create testsend.c

---
+++ testsend.c
@@ -0,0 +1,48 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc,char* argv[])
+{
+	if (argc<=2)
+	{
+		printf("usage: %d ip_address port\n",basename(argv[0]));
+		return 1;
+	}
+	
+	const char* ip=argv[1];
+	int port=atoi(argv[2]);
+	
+	struct sockaddr_in address;
+	bzero(&address,sizeof(address));
+	address.sin_family=AF_INET;
+	inet_pton(AF_INET,ip,&address.sin_addr);
+	address.sin_port=htons(port);
+	
+	int sock=socket(PF_INET,SOCK_STREAM,0);
+	assert(sock>=0);
+	
+	if (connect(sock,(struct sockaddr*)&address,sizeof(address))<0)
+	{
+		printf("connection failed\n");
+	}
+	else
+	{
+		const char* oob_data="abc";
+		const char* normal_data="123";
+		send(sock,normal_data,strlen(normal_data),0);
+		send(sock,oob_data,strlen(oob_data),MSG_OOB);
+		send(sock,normal_data,strlen(normal_data),0);
+		
+	}
+
+	close(sock);
+	return 0;
+
+}
